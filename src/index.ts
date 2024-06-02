9a0496657d517ac8da72a7be8ebd27430fc4a44f
YU <111847207+YU000jp@users.noreply.github.com>
2024-06-02 19:26:59 +0900

    feat(Add): translations full languages (Logseq)

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
-import '@logseq/libs' //https://plugins-doc.logseq.com/
+import '@logseq/libs'; //https://plugins-doc.logseq.com/
 import { SettingSchemaDesc } from '@logseq/libs/dist/LSPlugin.user'
-import { getDateForPage } from 'logseq-dateutils' //https://github.com/hkgnp/logseq-dateutils
-import { setup as l10nSetup, t } from "logseq-l10n" //https://github.com/sethyuan/logseq-l10n
-import Swal from 'sweetalert2' //https://sweetalert2.github.io/
+import { getDateForPage } from 'logseq-dateutils'; //https://github.com/hkgnp/logseq-dateutils
+import { setup as l10nSetup, t } from "logseq-l10n"; //https://github.com/sethyuan/logseq-l10n
+import Swal from 'sweetalert2'; //https://sweetalert2.github.io/
 import { logseq as PL } from "../package.json"
 import { closeModal, createReadingPage, openModal, RecodeDateToPage, setCloseButton, setMainUIapp } from './lib'
 import { checkAssets } from './toAssets'
+import af from "./translations/af.json"
+import de from "./translations/de.json"
 import en from "./translations/en.json"
+import es from "./translations/es.json"
+import fr from "./translations/fr.json"
+import id from "./translations/id.json"
+import it from "./translations/it.json"
+import ko from "./translations/ko.json"
+import nbNO from "./translations/nb-NO.json"
+import nl from "./translations/nl.json"
+import pl from "./translations/pl.json"
+import ptBR from "./translations/pt-BR.json"
+import ptPT from "./translations/pt-PT.json"
+import ru from "./translations/ru.json"
+import sk from "./translations/sk.json"
+import tr from "./translations/tr.json"
+import uk from "./translations/uk.json"
+import zhCN from "./translations/zh-CN.json"
+import zhHant from "./translations/zh-Hant.json"
 const pluginId = PL.id //set plugin id from package.json
 
 // Google Books API https://developers.google.com/books/docs/v1/using?hl=ja
@@ -20,7 +38,7 @@ const main = async () => {
   // i18n
   await l10nSetup({
     defaultLocale: "ja",
-    builtinTranslations: { en }
+    builtinTranslations: { en, af, de, es, fr, id, it, ko, "nb-NO": nbNO, nl, pl, "pt-BR": ptBR, "pt-PT": ptPT, ru, sk, tr, uk, "zh-CN": zhCN, "zh-Hant": zhHant }
   })
   /* user setting */
   // https://logseq.github.io/plugins/types/SettingSchemaDesc.html
@@ -232,8 +250,8 @@ const createBookPage = (FullTitle: string, data: any, selectedTitle: string, ope
 `)
             if (selectedBook.volumeInfo.description) {
               // 「#」が含まれる場合エスケープする
-              let desc:string = selectedBook.volumeInfo.description
-              desc = desc.replaceAll("#","# ")
+              let desc: string = selectedBook.volumeInfo.description
+              desc = desc.replaceAll("#", "# ")
               await logseq.Editor.prependBlockInPage(createPage.uuid, `
 #+BEGIN_QUOTE
 ${desc}
